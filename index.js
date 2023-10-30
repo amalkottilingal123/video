cf4b088f9f8f6be9c34f3fcff01206e3e01efefb
unknown <amalkottilingal@gmail.com>
2023-10-30 22:33:38 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+// import json server
+const jsonserver=require('json-server')
+//import cors
+const cors=require('cors')
+
+
+//create json server (first line)
+const server=jsonserver.create()
+
+//json type data -js data  -before request resolve
+const middleware=jsonserver.defaults()
+
+
+//create a router
+const router=jsonserver.router('db.json')
+
+//apply in server
+server.use(middleware)
+
+server.use(cors())
+server.use(router)
+
+const PORT=5000
+server.listen(PORT,()=>{
+    console.log(`__Json Server Started at port : ${PORT}__`);
+})
